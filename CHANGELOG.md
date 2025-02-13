22cc675990c4edcefbd9cfe6cc3d19e6f043ddc6
nstcactus <nstcactus@gmail.com>
2025-02-13 19:04:19 +0100

    fix: lint the body line length instead of the full lenght of the body

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Replace the `body-max-length` in favor of `body-max-line-length` to lint the body line length instead of the full
+  length of the body
+
 
 ## [1.1.0] - 2025-02-13
 
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default {
   rules: {
     'scope-case': [2, 'always', 'kebab-case'],
     'header-max-length': [2, 'always', 120],
-    'body-max-length': [1, 'always', 120],
+    'body-max-line-length': [1, 'always', 120],
     'type-enum': [2, 'always', [
         ...config.rules['type-enum'][2],
         'release',

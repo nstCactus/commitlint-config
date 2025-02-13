201aa7027ff3557f48b05b0fe8a8805d95a9ba4b
nstcactus <nstcactus@gmail.com>
2025-02-13 18:50:01 +0100

    feat(tooling): customize markdown linting rules

--- .remarkrc.cjs
+++ .remarkrc.cjs
@@ -1,1 +1,6 @@
-exports.plugins = ['@form8ion/remark-lint-preset'];
+exports.plugins = [
+  '@form8ion/remark-lint-preset',
+  ['remark-lint-no-shortcut-reference-link', false],
+  ['remark-lint-maximum-line-length', 120],
+  ['remark-lint-unordered-list-marker-style', '-'],
+];
